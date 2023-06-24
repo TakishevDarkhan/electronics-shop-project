27058ef1393540c8925d5ccf5dfa89368f84f95e
TakishevDarkhan <kereydarkhan@yandex.kz>
2023-06-24 23:28:36 +0600

    add item2 & test_item2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,33 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            value = value[:10]
+        self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        with open('../src/items.csv', 'r', encoding="windows-1251") as file:
+            file_reader = csv.DictReader(file)
+            for row in file_reader:
+                cls(row["name"], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(value):
+        return int(float(value))
+
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -9,3 +9,13 @@ def test_init():
     assert item1.price == 10000
     assert item1.quantity == 20
     assert item1.name == "Смартфон"
+
+
+def test_name():
+    assert len(Item.all) != 5
+    item = Item.all[0]
+    assert Item.name != "Смартфон"
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
